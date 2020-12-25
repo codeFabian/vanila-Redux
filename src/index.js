76feb61ae3c5a6ca44b991e0bf6efff5011a17b2
codeFabian <yolo.fabian.k@gmail.com>
2020-12-25 23:22:00 +0900

    :fire: 리덕스 기초공부
    Store 에 상태를 넣고, 리듀서로 상태를 변경할 수 있고, 리듀서안에 액션을 통해서 상태를 어떤값으로 변경할지 지정

--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.getElementById("span");
 
-const countModifier = (count = 0) => {
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  }
   return count;
 };
 
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
+
 console.log(countStore.getState());
