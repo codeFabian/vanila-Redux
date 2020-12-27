0c40fd602a802f17a9bde4f4852d98e2236b5f17
codeFabian <yolo.fabian.k@gmail.com>
2020-12-27 22:12:50 +0900

    detail screen studying redux

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,28 +1,14 @@
-// import React from "react";
-
-// const ToDo = ({ text }) => {
-//   return (
-//     <li>
-//       {text}
-//       <button>DEL</button>
-//     </li>
-//   );
-// };
-
-// const mapDispatchToProps = (dispatch, ownProps) => {
-//   console.log(ownProps);
-// };
-
-// export default connect(null, mapDispatchToProps)(ToDo);
-
 import React from "react";
 import { connect } from "react-redux";
 import { actionCreators } from "../store";
+import { Link } from "react-router-dom";
 
-function ToDo({ text, onBtnClick }) {
+function ToDo({ text, onBtnClick, id }) {
   return (
     <li>
-      {text} <button onClick={onBtnClick}>DEL</button>
+      <Link to={`/${id}`}>
+        {text} <button onClick={onBtnClick}>DEL</button>
+      </Link>
     </li>
   );
 }
